839843326e79d19c1eb247b5971cf085a74823b1
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2022-03-22 13:12:17 +0200

    Changing test parameters

--- HW1/ga/main.cpp
+++ HW1/ga/main.cpp
@@ -4,6 +4,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <execution>
 
 void test();
 void runExperiments();
@@ -24,47 +25,85 @@ void test()
 void runExperiments()
 {
     auto crossovers = {0.3, 0.5, 0.7};
-    auto mutations = {0.0001, 0.001, 0.005, 0.01, 0.05};
-    auto hyperMutations = {0.005, 0.01, 0.05, 0.1, 0.15};
-    auto elites = {0.0, 0.02, 0.04, 0.08, 0.1};
-    auto selectionPressure = {0.80, 1.0, 1.2, 2.0, 5.0, 8.0, 10.0};
+    auto mutations = {
+        // 0.0001, 
+        0.001,
+         0.005,
+          0.01,
+           0.05
+           };
+    auto elites = {
+        // 0.0, 
+    0.02,
+     0.04, 
+     0.08,
+     };
+    auto selectionPressure = {
+        0.80, 
+        1.0, 
+        1.2, 
+        5.0, 
+        10.0
+        };
     auto crossoverTypes = {
-        ga::CrossoverType::Chaotic,
+        // ga::CrossoverType::Chaotic,
         ga::CrossoverType::Classic,
-        ga::CrossoverType::Sorted,
+        // ga::CrossoverType::Sorted,
     };
     auto hillclimbings = {
         ga::HillclimbingType::BestImprovement,
-        ga::HillclimbingType::FirstImprovement,
-        ga::HillclimbingType::FirstImprovementRandom,
+        // ga::HillclimbingType::FirstImprovement,
+        // ga::HillclimbingType::FirstImprovementRandom,
     };
-    auto hyperMutationSteps = {5, 10, 20};
-    auto encodingChanges = {1, 2, 5, 10, 15, 20};
+    auto hyperMutationSteps = {
+        5, 
+        10, 
+        20,
+        };
+    auto encodingChanges = {
+        // 1, 
+    2, 
+    5, 
+    10, 
+    20
+    };
 
     auto i = 0;
     for (auto c : crossovers) {
         for (auto m : mutations) {
-            for (auto h : hyperMutations) {
                 for (auto e : elites) {
                     for (auto s : selectionPressure) {
                         for (auto ct : crossoverTypes) {
                             for (auto hc : hillclimbings) {
                                 for (auto hs : hyperMutationSteps) {
-                                    for (auto ec : encodingChanges) {
-                                        std::cout << i << '\n';
-                                        std::ofstream f{std::to_string(i) +
+                                    // why doesn't parallelization work
+                                    std::for_each(std::execution::unseq, 
+                                    encodingChanges.begin(), encodingChanges.end(),[&](auto ec) {
+                                        auto copy = i++;
+                                         std::cout << copy << '\n';
+                                        std::ofstream f{"experiments/" + std::to_string(copy) +
                                                         ".txt"};
-                                        for (auto exp = 0; exp < 5; ++exp) {
+                                        f << "Crossover : " << c << '\n';
+                                        f << "Mutation : " << m << '\n';
+                                        f << "hyperMutations : " << m * 10 << '\n';
+                                        f << "elites : " << e << '\n';
+                                        f << "selectionPressure : " << s << '\n';
+                                        f << "crossoverTypes : " << (int)ct << '\n';
+                                        f << "hillclimbings : " << (int)hc << '\n';
+                                        f << "hyperMutationSteps : " << hs << '\n';
+                                        f << "encodingChanges : " << ec << '\n';
+                                        for (auto exp = 0; exp < 3; ++exp) {
                                             ga::GeneticAlgorithm ga{
-                                                c,  m,    h,      e,    s,
+                                                c,  m,    m * 10,      e,    s,
                                                 ct, hc,   100,    10,   hs,
-                                                ec, 2000, "cf01", true, true};
-                                            f << ga.run() << '\n';
+                                                ec, 2000, "rastrigin_func", true, true};
+                                            auto rez = ga.run();
+                                            f << rez << '\n';
                                         }
-                                    }
+                                    });
+                                    
                                 }
                             }
-                        }
                     }
                 }
             }
