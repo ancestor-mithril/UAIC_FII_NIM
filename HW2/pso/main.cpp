b33f7da0492b98df037762bf59db43e988d29535
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2022-04-15 17:39:30 +0300

    Calling pso with all methods

--- HW2/pso/main.cpp
+++ HW2/pso/main.cpp
@@ -6,14 +6,31 @@
 void runDefault();
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-    // std::cout << cec22::sanity_check() << '\n';
+    std::cout << cec22::sanity_check() << '\n';
     runDefault();
     return 0;
 }
 
+void runFunction(std::string_view functionName, int dimensions)
+{
+    std::cout << std::endl;
+    auto pso = pso::getDefault(functionName, dimensions);
+    auto result = pso.run();
+    std::cout << functionName << ' ' << result << std::endl;
+}
+
 void runDefault()
 {
-    auto pso = pso::getDefault("zakharov_func", 10);
-    auto result = pso.run();
-    std::cout << "Result: " << result << std::endl;
+    runFunction("zakharov_func", 10);
+    runFunction("rosenbrock_func", 10);
+    runFunction("schaffer_F7_func", 10);
+    runFunction("rastrigin_func", 10);
+    runFunction("levy_func", 10);
+    runFunction("hf01", 10);
+    runFunction("hf02", 10);
+    runFunction("hf03", 10);
+    runFunction("cf01", 10);
+    runFunction("cf02", 10);
+    runFunction("cf03", 10);
+    runFunction("cf04", 10);
 }
